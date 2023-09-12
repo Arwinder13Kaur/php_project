848875279048b507082eb08556358b4b8157bf51
Arwinder13Kaur <144332639+Arwinder13Kaur@users.noreply.github.com>
2023-09-13 00:53:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="./css/index.css">
+<?php   include_once('./includes/header.php') ?>
+<section>
+<main>
+    <h1>Welcome to My Website</h1>
+</main>
+</section>
+<?php include_once('./includes/footer.php')  ?>
